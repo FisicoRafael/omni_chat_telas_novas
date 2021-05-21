b5f635a9d7717272c609981acb945963948e1385
FisicoRafael <rafaelfferreira1987@gmail.com>
2021-05-21 09:35:45 -0300

    21-05-segundo

--- lib/components/menu_customizado.dart
+++ lib/components/menu_customizado.dart
@@ -15,10 +15,12 @@ class _MenuCustomState extends State<MenuCustom>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
   late Animation<double> animation;
+  late List<String> listaMenuCon;
 
   @override
   void initState() {
     super.initState();
+    listaMenuCon = widget.listaMenu;
     controller = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 200));
     animation = Tween<double>(begin: 0, end: 150).animate(controller);
@@ -36,27 +38,41 @@ class _MenuCustomState extends State<MenuCustom>
 
   @override
   Widget build(BuildContext context) {
-    widget.listaMenu.insert(0, "Solicitar pausa para:");
-
     return Material(
         child: Container(
-          width: animation.value,
-          height: animation.value,
-          decoration: BoxDecoration(
-              color: corBranca,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15),
-                bottomRight: Radius.circular(15),
-                bottomLeft: Radius.circular(15),
-              ),
-              boxShadow: <BoxShadow>[
-                BoxShadow(color: corPreto, spreadRadius: 1)
-              ]),
-          child: Column(
-            children: [
-              Container(child: Text(widget.listaMenu[0]),)
-            ],
+      width: animation.value,
+      height: animation.value,
+      decoration: BoxDecoration(
+          color: corBranca,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+            bottomLeft: Radius.circular(15),
           ),
-        ));
+          boxShadow: <BoxShadow>[BoxShadow(color: corPreto, spreadRadius: 1)]),
+      child: Column(
+        children: [
+          Container(
+            height: 30,
+            child: Text("Solicitar pausa para:"),
+          ),
+          Container(
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: listaMenuCon.length,
+                itemBuilder: buildItem),
+          )
+        ],
+      ),
+    ));
+  }
+
+  Widget buildItem(context, index) {
+    return Container(
+      height: 30,
+      child: ListTile(
+        title: Text(listaMenuCon[index]),
+      ),
+    );
   }
 }
