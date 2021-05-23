7bcfc6d89fa08886d701f5dbd5a4336fb4e04268
FisicoRafael <rafaelfferreira1987@gmail.com>
2021-05-23 09:43:25 -0300

    23-05-primeiro

--- lib/screens/nome_do_agente.dart
+++ lib/screens/nome_do_agente.dart
@@ -17,7 +17,8 @@ double larguraTelaGlobal = 0.0;
 double alturaTelaGlobal = 0.0;
 double alturaAppBarGlobal = 0.0;
 
-class _CadastroAgenteState extends State<CadastroAgente> {
+class _CadastroAgenteState extends State<CadastroAgente>
+    with TickerProviderStateMixin {
   List<String> listaMenu = [
     "Almoço",
     "Back Office",
@@ -26,6 +27,14 @@ class _CadastroAgenteState extends State<CadastroAgente> {
     "Demostração",
     "Particular"
   ];
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,29 +88,93 @@ class _CadastroAgenteState extends State<CadastroAgente> {
                                         ),
                                         Expanded(
                                           child: Container(
-                                            
                                             decoration: BoxDecoration(
                                                 color: corBranca,
                                                 borderRadius: BorderRadius.only(
-                                                    topRight: Radius.circular(15),
+                                                    topRight:
+                                                        Radius.circular(15),
                                                     topLeft:
                                                         Radius.circular(15))),
+                                            child: Column(
+                                              children: [
+                                                Container(
+                                                  height:
+                                                      constraints.maxHeight *
+                                                          0.08,
+                                                  color: Colors.red,
+                                                ),
+                                                TabBar(
+                                                    unselectedLabelColor: corCinza,
+                                                    controller: _tabController,
+                                                    indicator: BoxDecoration(
+                                                        borderRadius:
+                                                            BorderRadius.only(
+                                                                topRight: Radius
+                                                                    .circular(
+                                                                        20),
+                                                                topLeft: Radius
+                                                                    .circular(
+                                                                        20)),
+                                                        color: corBlueGrey),
+                                                    labelColor: Colors.white,
+                                                    tabs: [
+                                                      Tab(
+                                                        child: Row(
+                                                          mainAxisSize: MainAxisSize.min,
+                                                          children: [
+                                                            Icon(Icons.person),
+                                                            Text(("Para outro Agente"))
+                                                          ],
+                                                        ),
+                                                      ),
+                                                      Tab(
+                                                        child: Row(
+                                                          mainAxisSize: MainAxisSize.min,
+                                                          children: [
+                                                            Icon(Icons.account_tree_rounded),
+                                                            Text(("Para uma Fila"))
+                                                          ],
+                                                        ),
+                                                      ),
+                                                    ]),
+                                              ],
+                                            ),
                                           ),
-                                        )
+                                        ),
                                       ],
                                     ),
                                     Container(
+                                      alignment: Alignment.center,
                                       margin: EdgeInsets.only(
                                           top: (constraints.maxHeight * 0.02) /
                                               2,
                                           left: constraints.maxWidth * 0.05),
-                                      height: constraints.maxHeight * 0.15,
+                                      height: constraints.maxHeight * 0.12,
                                       width: constraints.maxWidth * 0.89,
                                       decoration: BoxDecoration(
                                           color: corBlueGrey,
                                           borderRadius: BorderRadius.only(
                                               topLeft: Radius.circular(15),
                                               topRight: Radius.circular(15))),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(
+                                            Icons.arrow_back,
+                                            color: corBranca,
+                                          ),
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(left: 8),
+                                            child: Text(
+                                              "Transferir Atendimento",
+                                              style: TextStyle(
+                                                  color: corBranca,
+                                                  fontSize: 20),
+                                            ),
+                                          )
+                                        ],
+                                      ),
                                     )
                                   ],
                                 ),
